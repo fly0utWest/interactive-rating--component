49b58ca458dcc7960e660ba8d7834f4504e00eac
onearmedstranger <shealtov@gmail.com>
2023-08-23 16:24:44 +0300

    task completed

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-let button = document.getElementById("5");
-button.addEventListener("click", (event) => {
+let ratingTab = document.querySelector(".rating-tab");
+let responseTab = document.querySelector(".response-tab");
+let submit = document.querySelector(".rating-form__submit");
+let checked;
+
+const buttons = document.getElementsByClassName("rate__button");
+let buttonsArray = Array.from(buttons);
+
+for (let i = 0; i < buttonsArray.length; i++) {
+  buttonsArray[i].addEventListener("click", (event) => {
     event.preventDefault();
-    button.style.backgroundColor = "var(--orange)";
-    button.style.color = "#fff";
+    checked = buttonsArray[i].id;
+    buttonsArray[i].classList.add("rate__button_checked");
+    for (let j = 0; j < buttonsArray.length; j++) {
+      if (buttonsArray[j].id !== checked) {
+        buttonsArray[j].classList.remove("rate__button_checked");
+      }
+    }
+    console.log(checked);
+  });
+}
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (checked !== undefined) {
+    ratingTab.style.display = "none";
+    responseTab.style.display = "flex";
+    document.querySelector(".grade").innerHTML = checked + " ";
+  }
 });
